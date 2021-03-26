a99f3397f938f42bb5970a80c932a7e4e1e9f050
scr2em <scr2em@yahoo.com>
2021-03-26 14:46:19 +0200

    hotfix /unfollow

--- routers/followRouter.js
+++ routers/followRouter.js
@@ -4,15 +4,8 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 const { isAuthenticated, isAuthorized } = require("../middlewares/auth");
 
-router.post("/follow", isAuthenticated, async (req, res) => {
-  let { process = "follow", followingId } = req.body;
-
-  let prcoessTypes = ["follow", "unfollow"];
-  
-  if (!prcoessTypes.includes(process)) {
-    res.status(400).send({message: "Invalid process" });
-    return;
-  }
+router.post("/follow:followingId", isAuthenticated, async (req, res) => {
+    let {followingId} = req.params
   if (!mongoose.isValidObjectId(followingId)) {
     res.status(404).send({message: "Bad formatted ID" });
     return;
@@ -28,7 +21,7 @@ router.post("/follow", isAuthenticated, async (req, res) => {
   let userId = req.id;
 
   if (userId == followingId) {
-    res.status(400).send({message: `You can't ${process} yourself` });
+    res.status(400).send({message: `You can't follow yourself` });
     return;
   }
   User.updateOne(
@@ -43,5 +36,36 @@ router.post("/follow", isAuthenticated, async (req, res) => {
     }
   );
 });
+router.post("/unfollow:followingId", isAuthenticated, async (req, res) => {
+  let {followingId} = req.params
+if (!mongoose.isValidObjectId(followingId)) {
+  res.status(404).send({message: "Bad formatted ID" });
+  return;
+}
 
+let userTofollow = await User.findOne({ _id: followingId });
+
+if (!userTofollow) {
+  res.status(404).send({message: "Ops, couldn't find any user with this ID"});
+  return;
+}
+
+let userId = req.id;
+
+if (userId == followingId) {
+  res.status(400).send({message: `You can't unfollow yourself` });
+  return;
+}
+User.updateOne(
+  { _id: userId },
+  { $pull: { following: [followingId] } },
+  function (err, data) {
+    if (err) {
+      res.status(500).send({message: "Something went wrong" });
+    } else {
+      res.status(201).send({message: "You have unfollowed this user successfully"});
+    }
+  }
+);
+});
 module.exports = router;
